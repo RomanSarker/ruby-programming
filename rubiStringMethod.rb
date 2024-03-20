c40e10fe2b42a1551bf7b2c131d84f7be5672396
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 16:28:26 +0600

    1sr part method string

---
+++ rubiStringMethod.rb
@@ -0,0 +1,36 @@
+# usefull ruby string method
+# 1.length og a string//length method
+
+# string = "hi there"
+# puts string.length
+# we can also use size length best practice
+
+# 2.reverce method
+# puts "hi roman".reverse
+
+# 3.search for one or more characters of a string //include method
+# result true or false
+# string = "this is roman"
+# puts string.include?("roman")
+# puts string.include?("karim")
+
+# 4.replace characters in a string #substitude sub method
+
+# string = "This is Roman . Roman live bd"
+# puts string.sub("Roman", "Sarkers")
+
+# string = "This is Roman . Roman live bd"
+# puts string.gsub("Roman", "Sarkers") #g mean global
+
+# 5.split a string #split method
+
+# string = "This is Roman . Roman live bd"
+
+# puts string.split
+# p string.split //array akare dekhar jonno
+
+# 6.Trim a string //strip method
+
+string = "hi there       "
+# puts string.strip
+p string.strip
