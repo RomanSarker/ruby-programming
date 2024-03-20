69b2d86580a0894f42bb98368430da3acb67d7ed
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 15:07:28 +0600

    symbol and string

---
+++ diifSymbolString.rb
@@ -0,0 +1,13 @@
+# puts "Roman".object_id
+# puts "Roman".object_id
+# puts "Roman".object_id
+
+
+puts :Roman.object_id
+puts :Roman.object_id
+puts :Roman.object_id
+
+
+puts :RomanSarker.object_id
+
+# It means symbol take less memory allocation which is best..see detais rubi on rails
