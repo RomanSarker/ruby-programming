77ee1cd66483fc0c9b1577ce9756eb5d410f303e
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 07:35:20 +0600

    first commit

---
+++ index.rb
@@ -0,0 +1,12 @@
+
+puts "Enter the first number:"
+num1 = gets.chomp.to_i
+
+puts "Enter the second number:"
+num2 = gets.chomp.to_i
+
+sum = num1 + num2
+
+puts "The sum of #{num1} and #{num2} is: #{sum}"
+
+
