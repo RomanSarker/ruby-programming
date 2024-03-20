d4bd6c052ef2ac072ca56d480f4b6b06b4fd485b
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 14:32:47 +0600

    conversion data

---
+++ datatypeConversion.rb
@@ -0,0 +1,13 @@
+# num1 = "45".to_i 
+# string to integer
+num2 = 5.to_s 
+
+num3 = "60".to_f
+# int to string
+# puts num1.class
+# puts num2.class
+
+# puts num1+num2
+puts num2.class
+
+puts num3
