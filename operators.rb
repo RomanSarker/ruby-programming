18f978578681610752dac24cfee984bbd76fded0
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 13:03:38 +0600

    oprator sdiv mod

---
+++ operators.rb
@@ -0,0 +1,23 @@
+# puts 1+2
+num1 = 30
+num2 =2
+
+
+# puts num1*num2
+
+# div modulo vs reminder
+puts 15/3 
+# [/ modulo operator]
+
+puts 15%3
+# % reminder operator
+
+
+# reminder method
+
+# puts 15.remainder(5)
+
+# to get two both best
+
+
+puts 16.divmod(5)
