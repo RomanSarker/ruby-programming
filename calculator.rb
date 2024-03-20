faebb6481f7ee1ea385ef30cd1612e67b4693e97
Roman Sarker <romansarker2015@gmail.com>
2024-03-20 15:22:13 +0600

    calculate

---
+++ calculator.rb
@@ -0,0 +1,11 @@
+# simple calculator
+puts "Enter ur first number"
+num1 = gets.chomp().to_f
+# chomp method remove the new line
+# as by default string print kore so convert to integer of float
+puts "Enter 2nd number"
+
+num2 = gets.chomp().to_f
+
+puts num1 + num2
+
