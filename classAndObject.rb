d3e874280c9c7021c23dee970105ceca10b16b9a
Roman Sarker <romansarker2015@gmail.com>
2024-03-21 11:35:41 +0600

    ruby class and object

---
+++ classAndObject.rb
@@ -0,0 +1,16 @@
+class Car 
+    attr_accessor :name, :price
+end
+
+car1 = Car.new()
+car1.name = "xCorolla"
+car1.price = 50000
+
+# puts car1.name
+# puts car1.price
+
+car2 = Car.new()
+car2.name = "Maruti"
+car2.price = 1000000
+
+puts car2.price
